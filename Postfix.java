aa3127c0c54dd461c017fbb4417ddb2b8dd38a01
EHLINGER Thibaut <thibaut.ehlinger@inria.fr>
2019-03-08 14:00:50 +0100

    Postfix squelette

---
+++ Postfix.java
@@ -0,0 +1,14 @@
+import static java.lang.System.out;
+import java.util.Stack;
+import java.io.*;
+
+public class Postfix{
+
+	public static void main (String[] argv) throws IOException{
+		Stack stack = new Stack();
+		StreamTokenizer st = new StreamTokenizer(
+				new BufferedReader(
+				new InputStreamReader(System.in)));
+		}
+	}
+
